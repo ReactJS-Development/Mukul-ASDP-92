807b92b508d49b078cd99c20a96bb5f1aefaa21e
Mukulsharmahsr5 <mukulsharmahsr5@gmail.com>
2018-09-06 17:59:31 +0530

    new files added

---
+++ src/Test.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import Test1 from './Test1.js';
+
+class Test extends Component {
+  render() {
+    return (
+      <div>
+
+    <h1>Evolution of TVS Apache</h1>
+
+    <Test1 />
+
+      </div>
+    );
+  }
+}
+
+export default Test;
---
+++ src/Test1.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Test1 extends Component {
+  render() {
+    return (
+      <div>
+
+      <img height="400" width="400" alt="Apache RTR 160" src="one.jpg" />
+      <img height="400" width="400" alt="Apache RTR 180" src="two.jpg" />
+      <img height="400" width="400" alt="Apache 160 4v" src="three.jpg" />
+      <img height="400" width="400" alt="Apache 200 4v" src="four.jpg" />
+      <img height="400" width="400" alt="Apache RR 310" src="five.jpg" />
+
+      </div>
+    );
+  }
+}
+
+export default Test1;
